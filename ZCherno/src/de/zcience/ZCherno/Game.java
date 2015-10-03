e0dac4396b58139d3af47e41cc613b2450f8b843
Zcience <david.liebemann@gmail.com>
2015-10-03 22:21:12 +0200

    Implemented up to Ep.4 of the Game Programming Series by TheCherno

--- ZCherno/src/de/zcience/ZCherno/Game.java
+++ ZCherno/src/de/zcience/ZCherno/Game.java
@@ -1,18 +1,36 @@
 package de.zcience.ZCherno;
 
-public class Game implements Runnable {
+import java.awt.Canvas;
+import java.awt.Dimension;
+import java.awt.image.BufferStrategy;
 
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable {
+
+
+	private static final long serialVersionUID = 1L;
 	public static int width = 300;
 	public static int height = width / 16 * 9;
 	public static int scale = 3; // how much the Game will be upscaled
 
 	private Thread thread;
+	private JFrame frame;
+	private boolean running = false;
+	
+	public Game(){
+		Dimension size = new Dimension(width * scale, height * scale);
+		setPreferredSize(size);
+		
+		frame = new JFrame();
+	}
 
 	/**
 	 * Creates new Thread object and starts it. Attaches Current Game Object to
 	 * Thread.
 	 */
 	public synchronized void start() {
+		running = true;
 		thread = new Thread(this, "Display"); // attaching Thread to Game Object
 		thread.start();
 	}
@@ -21,6 +39,7 @@ public class Game implements Runnable {
 	 * Stops the thread.
 	 */
 	public synchronized void stop(){
+		running = false;
 		try {
 			thread.join();
 		} catch (InterruptedException e) {
@@ -30,7 +49,37 @@ public class Game implements Runnable {
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		while(running){
+			update();
+			render();
+		}
+	}
+	
+	public void update() {
+		
+	}
+	
+	public void render() {
+		BufferStrategy bs = getBufferStrategy();
+		if(bs == null){
+			createBufferStrategy(3); // triple buffering :D
+			return;
+		}
+	}
 
+
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.frame.setResizable(false); // don't make the window resizable
+		game.frame.setTitle("ZCherno");
+		game.frame.add(game);
+		game.frame.pack(); // size up the frame to the wanted size
+		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		game.frame.setLocationRelativeTo(null); // centers our window
+		game.frame.setVisible(true); // shows the window
+		
+		game.start();
+		
+		
 	}
 }
